3738fb23e97a5c065aaada6356a3777d21d17eac
mra00000 <mra00000@gmail.com>
2019-03-14 22:32:26 +0700

    Initial commit

---
+++ app/src/main/java/com/fpt/prm/bikeshare/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fpt.prm.bikeshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
